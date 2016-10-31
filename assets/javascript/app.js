3764a85a16c4e3001f2fa1a266624b85a0d70fb8
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2016-10-31 18:05:12 -0500

    updated html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,15 @@
 // initialize the .js document with this:
 $( document ).ready(function() {
 // need to click a button to start the game and the timer- onclick event
-//timer counts down from 120 seconds
+    $("").on("click", function (){
+// when the start button clicked, the div with the questions that was hidden is shown
+    });
+//timer counts down from 120 seconds and stops game at 120 seconds
+
 // a function/way to link buttons with correct answers to tabulate results
+
 // need an incrementer for both correct, incorrect and unanswered questions
+
 // maybe an if-else statement to tabulate them based on the correct answer
 // guessed
 
