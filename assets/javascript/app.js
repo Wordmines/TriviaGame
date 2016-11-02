69024e53e0424574646ab3ab5974d7f53ef94270
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2016-11-01 22:42:09 -0500

    updated js and html pages to register key events for radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,31 +14,55 @@ $( document ).ready(function() {
 
 // when one of the radio buttons is clicked, store the value of that button and update either the 
 // numCorrectChoice, numIncorrectChoice, or numUnanswered accordingly
- 
-
-
-
-// when I input these into the console individually, and then check a button, 
-// I get my button value returned in the console. it won't return anything/or I don't 
-// know how to get it to return anything from my .js file.
- 		$('input:radio[name=q2choice]:checked').val();
-
-
-// I wasn't sure if it needed to be inside an on-click event, so I tried doing this, which
-// didn't work:
- 	$('#questionOne').on("change", function (){
- 		$('input:radio[name=q1choice]:checked').val();
- 	 	});
-
-// I've also tried this method, for which I am getting something returned as an alert,
-// but I'm not sure how to store the value that is being returned. 
-        $('#questionOne input').on('change', function() {
-            alert($('input[name="q1choice"]:checked', '#questionOne').val());
+		$('#questionOne input').on('change', function() {
+            console.log($('input[name="q1choice"]:checked', '#questionOne').val());
         });
 
+        $('#questionTwo input').on('change', function() {
+            console.log($('input[name="q2choice"]:checked', '#questionTwo').val());
+        });
+        $('#questionThree input').on('change', function() {
+            console.log($('input[name="q3choice"]:checked', '#questionThree').val());
+        });
+        $('#questionFour input').on('change', function() {
+        	console.log($('input[name="q4choice"]:checked', '#questionFour').val());
+        });
+        $('#questionFive input').on('change', function() {
+        	console.log($('input[name="q5choice"]:checked', '#questionFive').val());
+        });
+        $('#questionSix input').on('change', function() {
+        	console.log($('input[name="q6choice"]:checked', '#questionSix').val());
+        });
+        $('#questionSeven input').on('change', function() {
+        	console.log($('input[name="q7choice"]:checked', '#questionSeven').val());
+        });
+        $('#questionEight input').on('change', function() {
+        	console.log($('input[name="q8choice"]:checked', '#questionEight').val());
+        });
+        $('#questionNine input').on('change', function() {
+        	console.log($('input[name="q9choice"]:checked', '#questionNine').val());
+        });
+        $('#questionTen input').on('change', function() {
+        	console.log($('input[name="q10choice"]:checked', '#questionTen').val());
+        });
 
-
-
+// create a "done" button that defers matching the questions with their answers
+        // Create a deferred object
+		var defer = $.Deferred();
+ 
+// Add handlers to be called when dfd is resolved
+		defer
+// .done() can take any number of functions or arrays of functions
+  		.done( [ fn1, fn2 ], fn3, [ fn2, fn1 ] )
+// We can chain done methods, too
+  .done(function( n ) {
+    $( "p" ).append( n + " we're done." );
+  });
+ 
+// Resolve the Deferred object when the button is clicked
+$( "button" ).on( "click", function() {
+  dfd.resolve( "and" );
+});
 
 // need an incrementer for both correct, incorrect and unanswered questions
 
