27593c19ef624092bae621c8629872386a92b26f
Catherine Crooke <Cat@Catherines-MacBook-Pro.local>
2016-11-04 16:47:59 -0500

    updated js and html files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,96 +1,197 @@
 // initialize the .js document with this:
 $( document ).ready(function() {
+
+	var game = {
+		questions: [
+		{
+	   		question: 'What is the capital of Vermont?', 
+	   		possibles: ['Battleboro', 'Montpelier', 'Barre City', 'Burlington'],
+	   		id: 'question-one',
+	   		answer: 1
+		}, {
+			question: 'What is the capital of Illinois?',
+			possibles: ['Springfield', 'Chicago', 'Rockford', 'Joliet', 'Evanston'],
+			id: 'question-two',
+			answer: 0
+		}, {
+			question: 'What is the capital of California?',
+			possibles: ['San Francisco', 'Los Angeles', 'Mendocino', 'San Diego', 'Sacramento'],
+			id: 'question-three',
+			answer: 4
+		}, {
+			question: 'What is the capital of Indiana?',
+			possibles: ['Fort Wayne', 'Indianapolis', 'Terre Haute', 'Bloomington', 'Indianapolis'],
+			id: 'question-four',
+			answer: 1
+		}, {
+			question: 'What is the capital of Alabama?',
+			possibles: ['Montgomery', 'Birmingham', 'Mobile', 'Tuscaloosa', 'Gulf Shores'],
+			id: 'question-five',
+			answer: 0
+		}, {
+			question: 'What is the capital of Colorado?',
+			possibles: ['Boulder', 'Denver', 'Vale', 'Fort Collins', 'Grand Junction'],
+			id: 'question-six',
+			answer: 1
+
+		}, {
+			question: 'What is the capital of Delaware?',
+			possibles: ['Wilmington', 'Newark', 'Dover', 'Lewes', 'Middletown'],
+			id: 'question-seven',
+			answer: 2
+		}, {
+			question: 'What is the capital of Florida?',
+			possibles: ['Miami', 'Tampa', 'Jacksonville', 'Daytona Beach', 'Talahassee'],
+			id: 'question-eight',
+			answer: 4
+		}, {
+			question: 'What is the capital of Idaho?',
+			possibles: ['Coeur dAlene', 'Idaho Falls', 'Boise', 'Meridian', 'Twin Falls'],
+			id: 'question-nine',
+			answer: 2
+		}, {
+			question: 'What is the capital of Michigan?',
+			possibles: ['Macinack Island', 'Lansing', 'Grand Rapids', 'Ann Arbor', 'Flint'],
+			id: 'question-ten',
+			answer: 1
+		}, {
+			question: 'What is the capital of Washington State?',
+			possibles: ['Seattle', 'Bellevue', 'Spokane', 'Olympia', 'Bellingham'],
+			id: 'question-eleven',
+			answer: 3
+		}, {
+			question: 'What is the best kind of coffee?',
+			possibles: ['Intelligentsia', 'Blue Bottle', 'starbucks', 'Caribou', 'none of the above'],
+			id: 'question-twelve',
+			answer: 4
+		}
+		]}
+
+	
 // need to click a button to start the game and the timer- onclick event
     $(".startGame").on("click", function (){
 // when the start button clicked, the div with the questions that was hidden is shown
-		$('.wrapperA').show();
+		$('.wrapper').show();
 		console.log('hello');
 
 		$(this).hide();
-    });
-//timer counts down from 120 seconds and stops game at 120 seconds and then shows wrapperB
-	// $('.timer')
-
-// this variable holds answers from radio buttons
-	var questionInputs = [];
-	console.log(questionInputs);
-
+});
 // when one of the radio buttons is clicked, store the value of that button and update either the 
 // numCorrectChoice, numIncorrectChoice, or numUnanswered accordingly
 	$('#questionOne input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-		questionInputs.push($('input[name="q1choice"]:checked', '#questionOne').val());
         console.log($('input[name="q1choice"]:checked', '#questionOne').val());
     });
-// this action stores the answer of the checked event into the empty questionInputs array
     $('#questionTwo input').on('change', function() {
-        questionInputs.push($('input[name="q2choice"]:checked', '#questionTwo').val());
         console.log($('input[name="q2choice"]:checked', '#questionTwo').val());
     });
     $('#questionThree input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-    	questionInputs.push($('input[name="q3choice"]:checked', '#questionThree').val());
         console.log($('input[name="q3choice"]:checked', '#questionThree').val());
     });
     $('#questionFour input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q4choice"]:checked', '#questionFour').val());
         console.log($('input[name="q4choice"]:checked', '#questionFour').val());
     });
     $('#questionFive input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q5choice"]:checked', '#questionFive').val());
         console.log($('input[name="q5choice"]:checked', '#questionFive').val());
     });
     $('#questionSix input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q6choice"]:checked', '#questionSix').val());
         console.log($('input[name="q6choice"]:checked', '#questionSix').val());
     });
     $('#questionSeven input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q7choice"]:checked', '#questionSeven').val());
         console.log($('input[name="q7choice"]:checked', '#questionSeven').val());
     }); 
     $('#questionEight input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q8choice"]:checked', '#questionEight').val());
         console.log($('input[name="q8choice"]:checked', '#questionEight').val());
     });
     $('#questionNine input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q9choice"]:checked', '#questionNine').val());
         console.log($('input[name="q9choice"]:checked', '#questionNine').val());
     });
     $('#questionTen input').on('change', function() {
-// this action stores the answer of the checked event into the empty questionInputs array
-        questionInputs.push($('input[name="q10choice"]:checked', '#questionTen').val());
         console.log($('input[name="q10choice"]:checked', '#questionTen').val());
     });
 
-	// create a "done" button that defers matching the questions with their answers
-        // by creating a "deferred" event
-	var deferEvent = $.Deferred();
- 
-	// Add handlers to be called when deferEvent is resolved
-	deferEvent;
-	// .done() can take any number of functions or arrays of functions
-  		// .done('questionInputs');
+function formTemplate(data) {
+	var qString = "<form id='questionOne'>"+ data.question +"<br>";
+	var possibles = data.possibles;
+	for (var i = 0; i < possibles.length; i++) {
+		var possible = possibles[i];
+		console.log(possible);
+		qString = qString + "<input type='radio' name='"+data.id+"' value='A'/>"+possible;
+	
+	}
+	return qString + "</form>";	
+}
+window.formTemplate = formTemplate;
 
-// Resolve the Deferred object when the button is clicked
-	$( "button" ).on( "click", function() {
-  		deferEvent.resolve(questionInputs);
-  		console.log(questionInputs);
-	});
+function buildQuestions(){
+	var questionHTML = ''
+	for (var i = 0; i<game.questions.length; i++) {
+		questionHTML = questionHTML + formTemplate(game.questions[i]);
+	}
+	$('#questions-container').append(questionHTML);
 
-// need an incrementer for both correct, incorrect and unanswered questions
+}
 
-// maybe an if-else statement to tabulate them based on the correct answer
-// guessed
+function isCorrect(question){
+	var answers = $('[name='+question.id+']');
+	var correct = answers.eq(question.answer);
+	var isChecked = correct.is(':checked');
+	return isChecked;
+}
 
-// onclick events needed to tabulate results for each answer:
-    $("").on("click", function (){
-    });
+
+buildQuestions();
+
+function resultsTemplate(question){
+	var htmlBlock = '<div>'
+	htmlBlock = htmlBlock + question.question + ': ' + isChecked;
+	return htmlBlock + "</div>";
+}
+
+function checkAnswers (){
+	var resultsHTML = '';
+	var guessedAnswers = [];
+	var correct = 0;
+	var incorrect = 0;
+	var unAnswered =0
+
+
+	for (var i = 0; i<game.questions.length; i++) {
+		// resultsHTML = resultsHTML + resultsTemplate(game.questions[i]);
+		if (isCorrect(game.questions[i])) {
+			correct++;
+		} else {
+			incorrect++;
+			if (checkAnswered(question)) {
+				incorrect++;
+			} else {
+				unAnswered++;
+			}
+		}
+
+	}
+	$('.results').html('correct: '+correct+ "<br>" +'incorrect: '+incorrect+ "<br>" +'unanswered: '+unAnswered);
+}
+
+function checkAnswered(question){
+	for (var i=0; i<game.questions.length; i++) {
+		if (checkAnswers<isCorrect;
+		unAnswered++;
+	}
+	// query for all inputs for this question
+	// if any are checked, return true
+	// if none are checked return false
+}
+
+checkAnswers();
+// select the done button 
+$('#doneButton').on("click", checkAnswers);
+// and click callback on the done button
+
+// simpleTimerSolution
+
+
+
+
 
 
 });
